c18fccf08814e432e00615faccf5a98d6b2ea494
PalakMoghariya <palakmoghariya4@gmail.com>
2024-08-14 18:17:50 +0530

    add file

--- src/components/ReactHook1.jsx
+++ src/components/ReactHook1.jsx
@@ -1,11 +1,18 @@
 // import { useState } from "react"
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function ReactHook1() {
     const[count,setCount]=useState(0);
+    useEffect(() => {
+      // Update the document title using the browser API
+      document.title = `You clicked ${count} times`;
+    });s
   return (
     <div>
-
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   )
 }
